89feb8456aa24d99dda402c7dba28e1af3dee176
milk wuz here <sooversoulo@gmail.com>
2026-07-20 21:14:35 +0000

    Configure Zsh and import shell aliases
    Added Zsh configuration and shell aliases import.

--- home/milk.nix
+++ home/milk.nix
@@ -5,15 +5,23 @@
   home.username = "milk";
   home.homeDirectory = "/home/milk";
 
-  # Standard user environment packages 
+  # Standard user environment packages
   home.packages = with pkgs; [
     git
     curl
   ];
 
+  # Force Zsh environment tracking rules to automatically map your custom aliases file
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    # Points cleanly to the aliases module in your repository root
+    shellAliases = import ../aliases.nix; 
+  };
+
   # Lets Home Manager manage your shell environment configuration settings
   programs.home-manager.enable = true;
 
   # Match this version directly to the core OS channel release tag (24.11)
-  home.stateVersion = "24.11"; 
+  home.stateVersion = "24.11";
 }
