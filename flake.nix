94eb61964dfca929b322cb8f0e9f87fa21a8257a
milk wuz here <sooversoulo@gmail.com>
2026-07-20 19:46:01 +0000

    Fix redundant inputs declaration in flake.nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "osyso Master System Orchestration Flake";
 
   inputs = {
-    inputs = {
+  
     # Core Base OS Layers
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
