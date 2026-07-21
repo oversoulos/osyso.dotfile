1ae6d11965a6b31f1bdca1ba6bf3b4a78903354c
milk wuz here <sooversoulo@gmail.com>
2026-07-21 00:26:58 +0000

    Update milk.nix

--- home/milk.nix
+++ home/milk.nix
@@ -1,28 +1,24 @@
 { config, pkgs, inputs, ... }:
-
 {
   # Ensure this matches the home directory path of your system user account
   home.username = "milk";
   home.homeDirectory = "/home/milk";
-
   # Standard user environment packages
   home.packages = with pkgs; [
     git
     curl
   ];
-
   # Force Zsh environment tracking rules to automatically map your custom aliases file
   programs.zsh = {
     enable = true;
     enableCompletion = true;
     # Points cleanly to the aliases module in your repository root
-    shellAliases = import ../aliases.nix; 
+    shellAliases = import ../aliases.nix;
   };
-
   # Lets Home Manager manage your shell environment configuration settings
   programs.home-manager.enable = true;
-   
-   wayland.windowManager.hyprland = {
+
+  wayland.windowManager.hyprland = {
     enable = true;
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     plugins = [
@@ -30,7 +26,10 @@
       pkgs.hyprglass-built
       pkgs.hyprlandPlugins.hyprexpo
     ];
+    extraConfig = builtins.readFile ../.config/hypr/hyprland.conf;
   };
+
+  xdg.configFile."hypr/pyprland.toml".source = ../.config/hypr/pyprland.toml;
 
   # Match this version directly to the core OS channel release tag (24.11)
   home.stateVersion = "24.11";
